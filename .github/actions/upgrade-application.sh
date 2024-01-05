4b94cd7e78cfbd7334ec712027941033e88fe303
withanage <dulip.withanage@gmail.com>
2024-01-05 06:04:34 +0100

    v1

--- .github/actions/upgrade-application.sh
+++ .github/actions/upgrade-application.sh
@@ -21,7 +21,10 @@ rm -rf files
 rm -rf public
 
 if [[ "$TEST" == "pgsql" ]]; then
+  echo " drop database \"${DBNAME}\" " >> $GITHUB_STEP_SUMMARY
   psql -c "DROP DATABASE \"${DBNAME}\""; -U postgres
+  echo " drop database \"${DBNAME}\" " >> $GITHUB_STEP_SUMMARY
+
   #psql -c "DROP USER \"${DBUSERNAME}\"" -U postgres
 elif [[ "$TEST" == "mysql" ]]; then
   sudo mysql -u root -e "DROP DATABASE  \`${DBNAME}\` ";
