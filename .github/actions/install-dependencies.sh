82b118bcad10984d12e768ecc9a6198988b69c4c
withanage <dulip.withanage@gmail.com>
2024-01-09 09:36:14 +0100

    v1

--- .github/actions/install-dependencies.sh
+++ .github/actions/install-dependencies.sh
@@ -13,3 +13,10 @@ set -e
 
 # Search for composer.json files, and run Composer to install the dependencies.
 find . -maxdepth 4 -name composer.json -exec bash -c 'composer --no-ansi --working-dir="`dirname {}`" install --optimize-autoloader' ";"
+
+# Install node modules
+npm i g -npm
+npm i @vue/cli-service
+npm i cypress
+npm install
+npm run build
