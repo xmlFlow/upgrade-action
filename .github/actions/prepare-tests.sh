409f66fa94a9d97710fbf5f799035ece26455fbd
withanage <dulip.withanage@gmail.com>
2024-01-09 06:31:38 +0100

    v1

--- .github/actions/prepare-tests.sh
+++ .github/actions/prepare-tests.sh
@@ -15,15 +15,15 @@ set -e
 # Set some environment variables.
 export BASEURL="http://localhost" # This is the URL to the installation directory.
 export DBHOST=localhost # Database hostname
-export DBNAME=${APPLICATION} # Database name
-export DBUSERNAME=${APPLICATION} # Database username
-export DBPASSWORD=${APPLICATION} # Database password
+export DBNAME=${APPLICATION}-ci # Database name
+export DBUSERNAME=${APPLICATION}-ci # Database username
+export DBPASSWORD=${APPLICATION}-ci # Database password
 export FILESDIR=files # Files directory (relative to application directory -- do not do this in production!)
 export DATABASEDUMP=~/database.sql.gz # Path and filename where a database dump can be created/accessed
 export FILESDUMP=~/files.tar.gz # Path and filename where a database dump can be created/accessed
 
 # Install required software
-sudo apt-get install -q -y libbibliotation-parser-perl libhtml-parser-perl
+sudo apt-get install -q -y libbiblio-citation-parser-perl libhtml-parser-perl
 
 # Create the database and grant permissions.
 if [[ "$TEST" == "pgsql" ]]; then
